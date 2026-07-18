50ec55a52f5327712eae483c966611d771de0e6b
morehidalgg0 <morena-hidalgo@estudiante.mdp.utn.edu.ar>
2026-07-18 00:40:08 -0300

    Replace GTM with Google Analytics tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -44,23 +44,18 @@ export default function RootLayout({
       className={`${outfit.variable} ${oswald.variable} h-full antialiased`}
     >
       <head>
-        <Script id="google-tag-manager" strategy="beforeInteractive">
-          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-KW6L7MGS');`}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-S0XZWJ6RFW"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-S0XZWJ6RFW');`}
         </Script>
       </head>
       <body className="min-h-full flex flex-col bg-neutral-950 text-white font-sans">
-        <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-KW6L7MGS"
-            height="0"
-            width="0"
-            style={{ display: "none", visibility: "hidden" }}
-          />
-        </noscript>
         {children}
       </body>
     </html>
