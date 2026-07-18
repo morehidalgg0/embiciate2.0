56ccf377a830522e45883a52ff5bfe7b22d1e552
morehidalgg0 <morena-hidalgo@estudiante.mdp.utn.edu.ar>
2026-07-18 00:34:29 -0300

    Add Google Tag Manager

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import { Outfit, Oswald } from "next/font/google";
 import "./globals.css";
 
@@ -42,7 +43,24 @@ export default function RootLayout({
       lang="es"
       className={`${outfit.variable} ${oswald.variable} h-full antialiased`}
     >
+      <head>
+        <Script id="google-tag-manager" strategy="beforeInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-KW6L7MGS');`}
+        </Script>
+      </head>
       <body className="min-h-full flex flex-col bg-neutral-950 text-white font-sans">
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-KW6L7MGS"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
         {children}
       </body>
     </html>
